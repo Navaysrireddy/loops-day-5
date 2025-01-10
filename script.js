50fa8c42b0c10d38be6e1f234fe446e48dbf1512
navyasrireddy-github <147521140+Navaysrireddy@users.noreply.github.com>
2025-01-10 16:47:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// // Task1
+// for(var i=1; i<=20; i++)
+//     {
+//         console.log(i);
+//     }
+    
+    
+    // // Task2
+    // for(var i=1; i<=10; i++)
+    //     {
+    //         console.log(i*i);
+    //     }
+    
+    // // Task3
+    // for(var i=1; i<=10; i++){
+    //     console.log(5*i);
+    // }
+    
+    // //Task 4
+    // for(var i=20; i>=1; i--)
+    // {
+    //     console.log(i)
+    // }
+    
+    // // Task5
+    // for(var i=20; i>=1; i=i-2)
+    //     {
+    //         console.log(i)
+    //     }
+    
+    // // Task6
+    // var arr = [10,20,30,40,50];
+    // for(var i=0; i<=arr.length-1;i++)
+    // {
+    //     console.log(arr[i])
+    // }
+    
+    
+    // Task7
+    var arr = ['apple','banana','cherry'];
+    for(var i=0; i<=arr.length-1;i++)
+    {
+        console.log(arr[i])
+    }
